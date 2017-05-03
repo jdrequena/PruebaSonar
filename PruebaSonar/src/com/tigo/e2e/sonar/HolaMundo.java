c51801ee49e68c04d8f24e1d543e280bd4061a5a
jdrequena <jdrequena@JDREQUENA-TF-LT.Comcel.com.gt>
2017-05-03 15:47:41 -0600

    prueba sonar

---
+++ PruebaSonar/src/com/tigo/e2e/sonar/HolaMundo.java
@@ -0,0 +1,16 @@
+package com.tigo.e2e.sonar;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		
+	//while(false){
+		System.out.println("Hola Mundo");
+	//	}
+		
+		
+			
+		
+	}
+
+}
