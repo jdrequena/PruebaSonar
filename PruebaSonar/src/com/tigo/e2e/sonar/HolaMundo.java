5f6320faea2b8014caf13b7c67521dedc9700c17
jdrequena <jdrequena@JDREQUENA-TF-LT.Comcel.com.gt>
2017-05-08 17:03:45 -0600

    pruebas de sonarqube

--- PruebaSonar/src/com/tigo/e2e/sonar/HolaMundo.java
+++ PruebaSonar/src/com/tigo/e2e/sonar/HolaMundo.java
@@ -1,16 +1,34 @@
 package com.tigo.e2e.sonar;
 
 public class HolaMundo {
-
+	
 	public static void main(String[] args) {
 		
 	//while(false){
-		System.out.println("Hola Mundo");
-	//	}
-		
-		
-			
-		
-	}
 
+		System.out.println("Hola Mundo");
+		 int [] array = new int[20];
+         array[-3] = 24;
+         int a = 23/0;	
+		try
+		{
+			array [-3]= 24;
+			
+		}
+		catch (ArrayIndexOutOfBoundsException excepcion)
+		{
+		while (true)
+		{
+			System.out.println(" Error de índice en un array");
+		
+		}
+		}
+		
+		}
+		
+		
+		
+	//	
+		
+	
 }
